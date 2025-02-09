dc65b85a190b4165bbfc6bb7950ad3ba98c669e9
CarlosRxD <carlosrcardoza634@gmail.com>
2025-02-09 07:11:44 -0600

    update2

--- src/main/java/game/utilities/GameKeyListener.java
+++ src/main/java/game/utilities/GameKeyListener.java
@@ -1,7 +1,6 @@
 package game.utilities;
 
 import game.principal.SnakeGame;
-
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -17,16 +16,6 @@ public class GameKeyListener extends KeyAdapter {
         // Tecla para salir
         if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
             System.exit(0);
-        }
-         //Controles para el jugador 1 (flechas)
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            game.getSnake1().setDirection("RIGHT");
-        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            game.getSnake1().setDirection("LEFT");
-        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
-            game.getSnake1().setDirection("UP");
-        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            game.getSnake1().setDirection("DOWN");
         }
         // Controles para el jugador 2 (WASD)
         if (e.getKeyCode() == KeyEvent.VK_D) {
